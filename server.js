07bca9762cb0eaea49996375312636ba989b2cc6
AminX <Amin.dehgan5656@gmail.com>
2020-01-23 20:45:25 +0330

    Init Author

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-
+const authorRouter = require('./routes/authors')
 const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true})
 const db = mongoose.connection;
 db.on('error', err => console.log(err))
@@ -22,5 +23,6 @@ db.on('error', err => console.log(err))
 db.on('open', () => console.log("Connected"))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
